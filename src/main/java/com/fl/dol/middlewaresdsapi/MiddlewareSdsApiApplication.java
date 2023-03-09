7cb0c300a9cdaa26e0e6b0786f711df981a5fe0a
MANISHK-GITHUB <107134833+MANISHK-GITHUB@users.noreply.github.com>
2023-03-08 21:41:36 -0500

    Init commit

--- src/main/java/com/fl/dol/middlewaresdsapi/MiddlewareSdsApiApplication.java
+++ src/main/java/com/fl/dol/middlewaresdsapi/MiddlewareSdsApiApplication.java
@@ -2,12 +2,41 @@ package com.fl.dol.middlewaresdsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class MiddlewareSdsApiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MiddlewareSdsApiApplication.class, args);
     }
+
+    @RequestMapping("/")
+    String sayHello() {
+        return "Hello World From Middleware Application !";
+    }
+
+    @PostMapping("/hello")
+    String sayHelloandHi() {
+        return "Its calling the Hi & Hello world Together From Middleware Application!";
+    }
+
+    @GetMapping("/getone")
+    public @ResponseBody ResponseEntity<String> get() {
+        return new ResponseEntity<String>("GET Response From Middleware Application", HttpStatus.OK);
+    }
+
+    @PostMapping("/testtwo")
+    public @ResponseBody ResponseEntity<String> posttwo() {
+        return new ResponseEntity<String>("POST Response From Middleware Application", HttpStatus.OK);
+    }
+
+
 
 }
