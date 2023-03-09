a7f71949553958194e34fec2ff908e58cc0fd17f
MANISHK-GITHUB <107134833+MANISHK-GITHUB@users.noreply.github.com>
2023-03-08 21:36:50 -0500

    Init commit

---
+++ src/main/java/com/fl/dol/middlewaresdsapi/MiddlewareSdsApiApplication.java
@@ -0,0 +1,13 @@
+package com.fl.dol.middlewaresdsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareSdsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddlewareSdsApiApplication.class, args);
+    }
+
+}
